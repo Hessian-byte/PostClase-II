0a1b7c4d3fd7c0df5e60dacabb72fe8254f77a6c
LORENZO <l.devia02@ufromail.cl>
2020-09-03 17:58:18 -0400

    Se eliminó el formato original de la gui para hacerla de otra forma.

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -1,11 +1,6 @@
 package main;
 
-import guis.VentanaPedidos;
-
 public class Launcher {
-    public static void main(String[] args) {
-        VentanaPedidos ventana = new VentanaPedidos();
-        ventana.setVisible(true);
-    }
+
 
 }
